92e4198aee36365bb3de2050d505f64c2ed441a6
Jake Zalesny <jrzalesny@gmail.com>
2024-10-08 15:17:42 -0600

    added message fixes and texts in page

--- src/app/Components/Message.tsx
+++ src/app/Components/Message.tsx
@@ -13,11 +13,11 @@ const Message: React.FC<MessageProps> = ({ children, variant = 'primary' }) => {
         'px-2 py-2 font-bold text-button transition-colors duration-200',
         {
             // Primary button styles
-            'bg-primary-button text-white hover:bg-primary-button-hover active:bg-white active:text-primary-button':
+            'bg-primary-button text-white active:bg-white active:text-primary-button':
                 variant === 'primary',
 
             // Secondary button styles  
-            'bg-secondary-button text-white hover:bg-secondary-button-hover active:bg-white active:text-secondary-button':
+            'bg-secondary-button text-white active:bg-white active:text-secondary-button':
                 variant === 'secondary',
         }
     );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Image from "next/image";
 import Button from './Components/Button';
+import Message from './Components/Message';
 
 export default function Home() {
   return (
     <div>
       <Button variant="primary">Primary Button</Button>
       <Button variant="secondary">Secondary Button</Button>
+      <Message variant="secondary">Primary Message <Button variant="secondary">X</Button></Message>
     </div>
   );
 };
