9210848f972905c81a7ee44112ca1a1d281e0e44
Jake Zalesny <jrzalesny@gmail.com>
2024-10-08 15:42:28 -0600

    fixed directory changes

---
+++ src/app/components/Message.tsx
@@ -0,0 +1,28 @@
+// Components/Message.tsx
+
+import React from 'react';
+import classNames from 'classnames';
+
+export interface MessageProps {
+    children: React.ReactNode;
+    variant?: 'primary' | 'secondary';
+}
+
+const Message: React.FC<MessageProps> = ({ children, variant = 'primary' }) => {
+    const messageClass = classNames(
+        'px-2 py-2 font-bold text-button transition-colors duration-200',
+        {
+            // Primary button styles
+            'bg-primary-button text-white active:bg-white active:text-primary-button':
+                variant === 'primary',
+
+            // Secondary button styles  
+            'bg-secondary-button text-white active:bg-white active:text-secondary-button':
+                variant === 'secondary',
+        }
+    );
+
+    return <div className={messageClass}>{children}</div>;
+};
+
+export default Message;
