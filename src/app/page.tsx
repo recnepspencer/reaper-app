5cd675cd1e0646c8257c2cad0de62b4564cb43f7
LolaCuadra <lola_cuadra1940@byui.edu>
2024-12-14 20:06:00 -0700

    landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,4 @@
-'use client';
+import ProfilePage from "./profile/page";
+import { useUser, SignInButton, SignOutButton } from '@clerk/nextjs';
 
-import Message from "./components/Message";
-import Image from 'next/image';
-
-export default function Home() {
-  return (
-    <div className="flex">
-      <Image 
-        src="/images/logo.svg" 
-        alt="logo" 
-        width={48} 
-        height={48} 
-        className="rounded-full object-cover inline-flex"
-      />
-      <Message variant="secondary">
-        <div className="flex items-center space-x-4">
-          <span>Nice words for myself</span>
-          <Image 
-            src="/images/login.jpg" 
-            alt="login-photo" 
-            width={48} 
-            height={48} 
-            className="rounded-full object-cover" 
-          />
-        </div>
-      </Message>
-    </div>
-  );
-}
+export default ProfilePage;
